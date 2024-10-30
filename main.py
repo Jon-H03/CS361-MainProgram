6b2f67c77994b3c45f45cd2b919c7441b83031b2
Jon Hirsch <jonathan.hirsch2011@gmail.com>
2024-10-29 22:10:47 -0500

    Add random haiku function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time
+import random
+
 
 class MainProgram:
 
@@ -20,13 +22,49 @@ class MainProgram:
         time.sleep(1)
         while True:
             if answer == "1":
-                # Call haiku function
-                pass
+                input_or_rand = input("1. Input Haiku Topic\n"
+                                      "2. Generate Random Haiku\n")
+                self.haiku(input_or_rand)
+                break
             elif answer == "2":
                 # Show past haikus
                 pass
             elif answer == "3":
-                break
+                # Exit haiku bot
+                print("Thank you for using Haiku Bot! Session will now be closed.\n")
+                return 1
             else:
                 "Number not recognized. Please enter 1, 2, or 3.\n"
                 answer = input("Please enter the corresponding number.\n")
+
+    def haiku(self, user_input):
+        if user_input == "1":
+            pass
+        if user_input == "2":
+            output = self.generate_random_haiku()
+            return output
+
+    def generate_random_haiku(self):
+        random_haikus = {
+            1: "Just because where you\n"
+               "Are doesn't seem right, doesn't\n"
+               "Mean that it is wrong",
+            2: "Cool, calm refreshing\n"
+               "Fast, slow, but always moving\n"
+               "All around the world",
+            3: "Dangerous and wild\n"
+               "Destroys all things in its wake\n"
+               "Can't be tamed and harsh"
+        }
+        random_num = random.randint(1, 3)
+        print(random_haikus[random_num])
+
+
+if __name__ == "__main__":
+    program = MainProgram()
+    program.greeting()
+
+    while True:
+        continue_loop = program.fulfill_answer()
+        if continue_loop:
+            break
